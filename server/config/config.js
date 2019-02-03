8462a050dd339e4d4f0b52eadfc3ecec8f3af35b
javixv <javier.fz@outlook.com>
2019-02-02 23:33:15 -0600

    NodeServer Completado

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,26 @@
  * PUERTO DE LA APLICACION
  */
 
- process.env.PORT = process.env.PORT || 3000;
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+    urlDB = 'mongodb://cafe-user:jav123@ds017553.mlab.com:17553/cafe';
+// }
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,38 +1,34 @@
-require('./config/config')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
- 
-app.get('/usuario', function (req, res) {
-  res.json('Hello World')
+
+
+app.use(require('./routes/usuario'));
+
+
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
 });
 
-app.put('/usuario/:id', function (req, res) {
 
-  let id = req.params.id;
-  res.json({
-    id
-  })
-})
 
-app.delete('/usuario', function (req, res) {
-  res.json('delete user')
-})
-
-app.post('/usuario', function (req, res) {
-
-  let body = req.body;
-  res.json({
-    persona : body
-  });
-})
- 
 app.listen(process.env.PORT, () => {
-    console.log(`Escuchado en el puerto ${ process.env.PORT}`)
-})
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
